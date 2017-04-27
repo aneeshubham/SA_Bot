fa2661382e73720be218550087d31b854de8bc22
aneeshubham <shubham.gupta@lntinfotech.com>
2017-04-27 13:03:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// app.get('/', onRequest);
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// function onRequest(request, response){
-  // express.static(path.join(__dirname, 'public'));
-  // response.sendFile(path.join(__dirname, 'public/index.html'));
-// }
 
 
 var varAuth = 'Basic YWthc2hkZWVwOmxudExOVDJLMTZfMQ==';
@@ -23,13 +16,6 @@ var varHost = 'acs.crm.ap2.oraclecloud.com';
 var varPath = '/salesApi/resources/latest/Task_SA_c';
 const https = require('https');
 
-
-
-
-//Start CHat bot
-//var apiai = require('apiai');
- 
-//var appAI = apiai("f87977b15fba4fc9b49437df62c05b1e");
 
 app.post('/inputmsg',function(request,response){
 	console.log('Req : '+ request.body.result.parameters.any);
@@ -66,11 +52,18 @@ app.post('/inputmsg',function(request,response){
 		if(request.body.result.parameters.attribute){
 			var answer = responseObject.items[0][request.body.result.parameters.attribute];
 			console.log(" Value : " + answer);
-			var speech = "Your answer is "+ answer+".";
+			if(request.body.result.parameters.attribute=="AssignedTo_SA2_c")
+			{
+				var speech = "The task "+recordNameURL+"is assigned to"+ answer+".";
+			}
+			else
+			{
+				var speech = "Your answer is "+ answer+".";
+			}	
 		}
 		else{
 				var speech = "Couldnt find your answer";
-		}				     
+		}			     
 		return response.json({
 			speech: speech,
 			displayText: speech,
@@ -83,15 +76,6 @@ app.post('/inputmsg',function(request,response){
 	}).on('error', function(e){
 	console.error(e);
   });
-	
-	// var request = appAI.textRequest( res.result.resolvedQuery, {
-		// sessionId: '1234567890'
-	// });
-	
-	// request.on('response', function(res) {
-		// console.log("Res : "+res.result.parameters);
-		// response.send(  res.result.parameters );
-	// });
 
 	request.on('error', function(error) {
 		console.log(error);
@@ -101,49 +85,6 @@ app.post('/inputmsg',function(request,response){
 	
 });
 
-// app.post('/inputmsg',function(request,response){
-
-	
-// });
-
-
-//End of chatbot
-
-// app.post('/getObject',function(request,response){
-	// console.log(" RecordName :" + request.body.RecordName);
-    // var recordNameURL = (request.body.any).trim().replace( / /g, "%20" );
-    // console.log("recordNameURL :" + recordNameURL)
-	// var options = {
-		// host: varHost,
-		// port: 443,
-		// path: varPath + "/?q=RecordName=" + recordNameURL, 
-		// headers: {
-		// 'Authorization': varAuth,
-		// 'Content-Type': 'application/vnd.oracle.adf.resourceitem+json'
-		// }   
-	// };
-	// var responseObject;
-	// var r = https.get(options, function(res){
-	// var body = "";
-
-	// res.on('data', function(data) {
-		// body += data;
-	// });
-	// res.on('end', function() {
-		
-		// responseObject = JSON.parse(body);
-		// response.json(responseObject);
-        // console.log(" Object : " + body);
-        // console.log(" Attribute : " + request.body.attribute);
-		// console.log(" Value : " + responseObject.items[0]['Id']);
-		// console.log(" Value : " + responseObject.items[0][request.body.attribute]);
-        // response.json(responseObject.items[0][request.body.attribute]);
-
-		// })
-	// }).on('error', function(e){
-	// console.error(e);
-  // });
-// });
 
 function send404(response){
 	response.writeHead(404, {'Context-Type' : "text/plain"});
